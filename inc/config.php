0288a905d57426c4fa9ab40ba1161f1267ea16cb
Leila <6630467+mihokaenbyou@users.noreply.github.com>
2024-11-06 18:36:46 +0100

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -1,8 +1,8 @@
 <?php 	
 // connect to bdd
 	try {
-		// $bdd = new PDO('mysql:host=localhost;dbname=fuyuneko', 'root', '');
-		$bdd = new PDO('mysql:host=localhost;dbname=vulcania', 'vulcania', '1337');
+		// $bdd = new PDO('mysql:host=localhost;dbname=xxxx', 'xxxx', '');
+		$bdd = new PDO('mysql:host=localhost;dbname=xxxx', 'xxxx', 'xxxx');
 	}
 	catch (Exception $e) {
 	die('Error: ' . $e->getMessage());
